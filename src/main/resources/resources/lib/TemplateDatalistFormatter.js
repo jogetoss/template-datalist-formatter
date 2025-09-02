6d9199ac5f9629fdf611689bd2b2ddfbd180926a
Simnsh <149747294+Simnsh@users.noreply.github.com>
2025-09-02 20:19:35 +0800

    Add CodeMirror (DX9) support in TemplateDatalistFormatter and clean up debug logs
    Implemented detection for CodeMirror editor in DX9
    Maintained backward compatibility with Ace Editor (DX8)
    Updated insertFieldIntoTemplate and refreshPreview methods
    Removed console.log debug statements

--- src/main/resources/resources/lib/TemplateDatalistFormatter.js
+++ src/main/resources/resources/lib/TemplateDatalistFormatter.js
@@ -8,19 +8,18 @@
         css += "#previewBody #templateDatalistContent{ font-size: revert; }\n";
         css += "#fieldSelectionWrapper{ padding:10px; border: 1px solid grey; }\n"
         css += ".fieldSelection { border: 1px dotted; padding: 3px; margin: 3px; display: inline-block; cursor: pointer;}";
-        
-        console.log("renderField");
 
         var fields = "Columns Selection<br/><div id=\"fieldSelectionWrapper\">";
         
         //get current column name
         var currentColumnName = $(editor).find("div.property-editor-property[property-name='name']").find("input").val();
-        
+
         //get columns chosen so far
         Object.keys(DatalistBuilder.chosenColumns).forEach(key => {
             var fieldID = key;
             var fieldName = DatalistBuilder.chosenColumns[key].name;
             var fieldLabel = DatalistBuilder.chosenColumns[key].label;
+
             
             //cannot choose own column, will hit infinite loop
             if(currentColumnName != fieldName){
@@ -35,7 +34,7 @@
     initScripting : function() {
         var thisObj = this;
         var editor = "#" + thisObj.editorId;
-        
+
         $(editor).find("div.property-editor-property[property-name='css']").find("textarea").keyup(thisObj.throttle(function(){
            thisObj.refreshPreview(); 
         }));
@@ -47,8 +46,10 @@
         $("#" + thisObj.id + "_input").find(".fieldSelection").on("click", function(){
             thisObj.insertFieldIntoTemplate(this);
         });
-        
-        setTimeout(thisObj.refreshPreview(), 1000);
+
+        // DX9
+        setTimeout(() => thisObj.refreshPreview(), 1000);
+
     },
     getData : function(useDefault) {
         return "";
@@ -58,29 +59,78 @@
     },
     /*custom methods*/
     refreshPreview: function(){
-        var thisObj = this;
-        var editor = "#" + thisObj.editorId;
-        
-        console.log("refreshPreview" + editor);
+      var thisObj = this;
+      var editorRoot = "#" + thisObj.editorId;
 
-        var cssEditorID = $(editor).find("div.property-editor-property[property-name='css']").find(".ace_editor").attr("id");
-        var templateEditorID = $(editor).find("div.property-editor-property[property-name='template']").find(".ace_editor").attr("id");
-        
-        var css = ace.edit(cssEditorID).getValue();
-        var template = ace.edit(templateEditorID).getValue();
-        
-        $(editor).find("#previewBody style").html(css);
-        $(editor).find("#previewBody #templateDatalistContent").html(template);
+      // --- helpers to read editor content (Ace, CodeMirror, or textarea) ---
+      function readFieldValue(rootSel, propName) {
+        var $prop = $(rootSel).find("div.property-editor-property[property-name='" + propName + "']");
+
+        // 1) Ace (DX8)
+        var aceId = $prop.find(".ace_editor").attr("id");
+        if (aceId && window.ace) {
+          try {
+            return ace.edit(aceId).getValue();
+          } catch (e) {
+            console.warn("Ace read failed for", propName, e);
+          }
+        }
+
+        // 2) CodeMirror (DX9)
+        var cmEl = $prop.find(".CodeMirror").get(0);
+        if (cmEl && cmEl.CodeMirror) {
+          try {
+            return cmEl.CodeMirror.getValue();
+          } catch (e) {
+            console.warn("CodeMirror read failed for", propName, e);
+          }
+        }
+
+        // 3) Fallback: textarea (in case editor not initialized yet)
+        var ta = $prop.find("textarea").get(0);
+        if (ta) return ta.value;
+
+        // Nothing found
+        console.warn("No editor found for property", propName);
+        return "";
+      }
+
+      var css      = readFieldValue(editorRoot, "css");
+      var template = readFieldValue(editorRoot, "template");
+
+      // Update preview
+      $(editorRoot).find("#previewBody style").html(css);
+      $(editorRoot).find("#previewBody #templateDatalistContent").html(template);
     },
     insertFieldIntoTemplate: function(field){
         var thisObj = this;
         var editor = "#" + thisObj.editorId;
         var columnId = $(field).attr("attr-id");
         var columnName = $(field).attr("attr-name");
-        
-        var templateEditorID = $(editor).find("div.property-editor-property[property-name='template']").find(".ace_editor").attr("id");
-        templateEditor = ace.edit( templateEditorID );
-        templateEditor.session.insert(templateEditor.getCursorPosition(), "{" + columnId + "::" + columnName + "}");
+
+        // First try Ace(DX8)
+        var aceEditorID = $(editor).find("div.property-editor-property[property-name='template']").find(".ace_editor").attr("id");
+        if (aceEditorID) {
+            templateEditor = ace.edit( aceEditorID );
+            templateEditor.session.insert(templateEditor.getCursorPosition(), "{" + columnId + "::" + columnName + "}");
+            return;
+        }
+
+        // Try CodeMirror(DX9)
+            // Then try CodeMirror (DX9)
+            var cmElement = $(editor)
+                .find("div.property-editor-property[property-name='template']")
+                .find(".CodeMirror")
+                .get(0);
+
+            if (cmElement && cmElement.CodeMirror) {
+                var cm = cmElement.CodeMirror;
+                cm.replaceSelection("{" + columnId + "::" + columnName + "}");
+                cm.focus();
+                return;
+            }
+
+            console.warn("No supported editor found for template field.");
     },
     throttle: function(f, delay){
         var timer = null;
