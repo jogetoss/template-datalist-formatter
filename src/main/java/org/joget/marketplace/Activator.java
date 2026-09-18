f6242080aaef48a7135ff6e1f08b7b9625813b4a
nabila861 <jahannabila690@gmail.com>
2026-09-18 08:53:20 +0800

    Bump version to 9.0.2 in Activator

--- src/main/java/org/joget/marketplace/Activator.java
+++ src/main/java/org/joget/marketplace/Activator.java
@@ -8,7 +8,7 @@ import org.osgi.framework.ServiceRegistration;
 
 public class Activator implements BundleActivator {
 
-    public static final String VERSION = "9.0.0";
+    public static final String VERSION = "9.0.2";
     protected Collection<ServiceRegistration> registrationList;
 
     public void start(BundleContext context) {
